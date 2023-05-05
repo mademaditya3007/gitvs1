6d313b9f9274c3f0d7eeaac0ffe611a85e80423b
mademaditya3007 <mademaditya3007@gmail.com>
2023-05-05 13:39:25 +0530

    k1

---
+++ k1.java
@@ -0,0 +1,9 @@
+class k1{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        
+
+    }
+}

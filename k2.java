44be69a9fe8ad33f80681f05760920a15db4396e
mademaditya3007 <mademaditya3007@gmail.com>
2023-05-05 13:49:53 +0530

    update 1.0.11

--- k2.java
+++ k2.java
@@ -2,6 +2,7 @@ public class k2 {
     public static void main(String args[]){
         System.out.println("helllo");
         System.out.println(args[0]);
+        //thus will give the imput during compilation of jvm
         System.out.println(args[1]);
 
     }

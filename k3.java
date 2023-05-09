ee38eddc35f933be2563bdcc1d23ac28d572d908
mademaditya3007 <mademaditya3007@gmail.com>
2023-05-09 21:31:26 +0530

    update

---
+++ k3.java
@@ -0,0 +1,13 @@
+public class k3 {
+    public static void main(String args[]){
+        int a=5;
+        
+        int b=a++ + ++a + ++a + a++ + --a + a--;
+        System.out.println(args[0]);
+        
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ k4.java
@@ -0,0 +1,14 @@
+
+
+public class k4 {
+    public static void main(String args[]){
+        int a=10;
+        int b=10;
+
+        boolean result =a==b;
+        
+        System.out.println(a>b);
+        System.out.println(a==b);
+
+    }
+}

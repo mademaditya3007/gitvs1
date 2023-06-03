0d8421de4bc5b686af0bdbd40423acb675dbafce
mademaditya3007 <mademaditya3007@gmail.com>
2023-06-03 13:26:08 +0530

    pract

---
+++ k56.java
@@ -0,0 +1,81 @@
+// class k56{
+//     int a;
+//     String name;
+//     public static void main(String args[]){
+//         k56 obj=new k56();
+//         k56 obj2=new k56();
+//         obj.name="ditya";
+//         obj.a=3;
+
+//         System.out.println(obj.a);
+//         System.out.println(obj.name);
+//         System.out.println(obj2.a);
+//         System.out.println(obj2.name);
+
+
+//     }
+// }
+/*
+ * 
+ class calc{
+     public int add(int n1,int n2,int n3){
+         return (n1+n2+n3);
+         
+        }
+        public int add(int n1,int n2){
+            return (n1+n2);
+            
+        }
+    }
+    public class k56{
+        public static void main(String[] args) {
+            calc nus=new calc();
+        int result =nus.add(3,4);
+        System.out.println(result);
+
+    }
+}
+  
+*//*
+class s{
+    public void show(int a){
+        System.out.println("int"+a);
+    }
+    public void show(short a){
+        System.out.println("short"+a);
+    }
+    public void show(byte a){
+        System.out.println("byte"+a);
+    }
+    public void show(double a){
+        System.out.println("souble"+a);
+    }
+    public void show(char a){
+        System.out.println("char"+a);
+    }
+    public void show(float a){
+        System.out.println("flaot"+a);
+    }
+}
+public class k56{
+    public static void main(String[] args) {
+        s obj=new s();
+        char a=1;
+
+        obj.show(a);
+        
+    }
+}
+    */
+class k56{
+    public void show(int a){
+        System.out.println("int " +a);
+
+    }
+    public static void main(String args[]){
+        k56 obj=new k56();
+        byte a=1;
+        obj.show(a);
+
+    }
+}
